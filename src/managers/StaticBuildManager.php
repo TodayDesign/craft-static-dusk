ef38819fe91d828ad161645edcc580ca5b2964e4
Jonathan Lovera <jonathan.lovera@today.design>
2024-09-11 17:39:23 +1000

    Fix getScheduledBuilds throwing errors when using  AWS Code build
    Fix getScheduledBuilds throwing errors when using  AWS Code build

--- src/managers/StaticBuildManager.php
+++ src/managers/StaticBuildManager.php
@@ -17,44 +17,46 @@ class StaticBuildManager {
     {
         $settings = CraftStaticDusk::$plugin->getSettings();
 
-        $payload = [
-            'secret' => Craft::parseEnv($settings->webHookSecret),
-            'repo' => Craft::parseEnv($settings->gitRepo),
-            'ref' => Craft::parseEnv($settings->gitRef),
-            'envName' => Craft::parseEnv($settings->environmentName),
-            'site' => $site
-        ];
-
-        $response = null;
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => Craft::parseEnv($settings->webHookUrl) . '/scheduled',
-//             CURLOPT_URL => "http://host.docker.internal:3000/static-build/scheduled",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_POSTFIELDS => json_encode((object)$payload),
-            CURLOPT_VERBOSE => TRUE,
-        ));
-
-        $response = curl_exec($curl);
-
-        if ($response === false) {
-            return [];
-        }
-
-        $response = json_decode($response);
-
-        curl_close($curl);
-
-        if (property_exists($response, "responseObject")) {
-
-            // Convert unix milliseconds to seconds
-            $data = array_map(function ($build) {
-                $build->LaunchTime = $build->LaunchTime / 1000;
-                return $build;
-            }, $response->responseObject);
-
-            return $response->responseObject;
+        if (Craft::parseEnv($settings->webHookType) === 'GH') {
+            $payload = [
+                'secret' => Craft::parseEnv($settings->webHookSecret),
+                'repo' => Craft::parseEnv($settings->gitRepo),
+                'ref' => Craft::parseEnv($settings->gitRef),
+                'envName' => Craft::parseEnv($settings->environmentName),
+                'site' => $site
+            ];
+    
+            $response = null;
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => Craft::parseEnv($settings->webHookUrl) . '/scheduled',
+                // CURLOPT_URL => "http://host.docker.internal:3000/static-build/scheduled",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_POSTFIELDS => json_encode((object)$payload),
+                CURLOPT_VERBOSE => TRUE,
+            ));
+    
+            $response = curl_exec($curl);
+    
+            if ($response === false) {
+                return [];
+            }
+    
+            $response = json_decode($response);
+    
+            curl_close($curl);
+    
+            if (property_exists($response, "responseObject")) {
+    
+                // Convert unix milliseconds to seconds
+                $data = array_map(function ($build) {
+                    $build->LaunchTime = $build->LaunchTime / 1000;
+                    return $build;
+                }, $response->responseObject);
+    
+                return $response->responseObject;
+            }
         }
 
         return [];
